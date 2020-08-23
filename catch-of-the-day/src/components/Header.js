7fc5cccacf387e5dcf9b16f8ef48a9f6088290a8
AndrewSimons3 <simdrew07@gmail.com>
2020-08-23 12:49:30 -0500

    order inventory app components added

---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="Top">
+        <h1>Catch
+          <span className="ofThe">
+            <span className="Of">Of</span>
+            <span className="the">the</span>
+          </span>
+          Day
+        </h1>
+        <h3 classname="tagline">
+          <span>Fresh Dailey</span>
+        </h3>
+      </header>
+      
+
+    )
+  }
+}
+
+export default Header;
