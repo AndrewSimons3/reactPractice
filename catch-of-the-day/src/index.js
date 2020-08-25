d980226e11c9fe0dbb69daf144a2bf5fba82f8c1
AndrewSimons3 <simdrew07@gmail.com>
2020-08-25 07:48:39 -0500

    added router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import Router from "./components/Router";
 import { render } from 'react-dom';
-import App from './components/App';
 import "./css/style.css";
 
 
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
