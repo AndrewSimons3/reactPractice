a2231cabddb95f3fa66f5c48d96f07d791be56ed
AndrewSimons3 <simdrew07@gmail.com>
2020-08-26 18:19:08 -0500

    added helper functions

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { getFunName } from '../helpers';
 
 
 class StorePicker extends React.Component {
-  render() {
-    return <form className="store-selector">
+  goToStore() {
+    console.log("going to store")
+  }
+    render() {
+    return <form className="store-selector" onSubmit={this.goToStore}>
       <h2>
         Please Enter a Store
       </h2>
-      <input type="text" required placeholder="Store Name" />
+      <input type="text" 
+      required 
+      placeholder="Store Name"
+      defaultValue={getFunName()} />
       <button type="Submit">Visit Store</button>
     </form>
   }
